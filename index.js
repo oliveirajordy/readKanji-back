65b942a5490efecb6c9a7bfa244ff12519f19808
oliveirajordy <oliveira.jordy03@gmail.com>
2019-08-09 16:40:42 -0300

    add static .dat files route

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.use('/static',express.static(__dirname+'/static'))
+
 app.post('/converter', converter)
 
 app.listen(3001, console.log('ouvindo na porta 3001'))
